ee2b53e019196a5ad3c27296feb5bd9d287df088
Vivek Yadav <vivekyadav36837@gmail.com>
2022-06-24 16:58:02 +0530

    Update

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrl')
 
-app.listen(process.env.PORT || 3000);
+const app = express();
+app.use(express.urlencoded({extended: false}));
+mongoose.connect('mongodb://localhost:27017/urlShortener',{
+    useNewUrlParser:true,useUnifiedTopology:true
+});
+
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', async (req, res) => {
+  const shortUrls = await ShortUrl.find()
+  res.render('home', { shortUrls: shortUrls })
+})
+
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl })
+
+  res.redirect('/')
+})
+
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+  if (shortUrl == null) return res.sendStatus(404)
+
+  shortUrl.clicks++
+  shortUrl.save()
+
+  res.redirect(shortUrl.full)
+})
+
+app.listen(process.env.PORT || 4000);
